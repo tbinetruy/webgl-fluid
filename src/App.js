0edf59e791b014659c13999068b0cf2b32f16ec8
Thomas Binetruy <tbinetruy@gmail.com>
2018-02-20 14:41:27 -0500

    added webgl dependency

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import { Shaders, Node, GLSL } from "gl-react"
+import { Surface } from "gl-react-dom"
+
+const shaders = Shaders.create({
+  fluid: {
+    frag: GLSL`
+precision highp float;
+varying vec2 uv;
+uniform float blue;
+void main() {
+    gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
+}`
+  }
+})
+
+class Fluid extends React.Component {
+  render() {
+    const { blue } = this.props
+    return <Node
+      shader={shaders.fluid}
+      uniforms={{ blue }}
+    />;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Surface width={300} height={300}>
+        <Fluid blue={0.5} />
+      </Surface>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
