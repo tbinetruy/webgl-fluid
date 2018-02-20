a69df0eb37ff66b5ae6b3cc8a2712c0fd800f34b
Thomas Binetruy <tbinetruy@gmail.com>
2018-02-20 15:16:06 -0500

    refactored

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import React, { Component } from 'react'
 import './App.css'
+import Fluid from './Fluid.jsx'
 
 import { Shaders, Node, GLSL } from "gl-react"
 import { Surface } from "gl-react-dom"
 
-const shaders = Shaders.create({
-  fluid: {
-    frag: GLSL`
-precision highp float;
-varying vec2 uv;
-uniform float blue;
-void main() {
-    gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
-}`
-  }
-})
-
-class Fluid extends React.Component {
-  render() {
-    const { blue } = this.props
-    return <Node
-      shader={shaders.fluid}
-      uniforms={{ blue }}
-    />;
-  }
-}
-
 class App extends Component {
+  constructor(props) {
+      super(props)
+
+  }
+
   render() {
     return (
-      <div className="App">
-      <Surface width={300} height={300}>
-        <Fluid blue={0.5} />
-      </Surface>
-      </div>
+        <Fluid />
     )
   }
 }
